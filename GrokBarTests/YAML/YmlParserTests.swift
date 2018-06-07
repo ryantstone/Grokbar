01e71648025d380bbe85302c8f8066fb894cd958
Ryan Stone <ryan@ryanstone.io>
2018-06-07 15:45:52 -0400

    added invalid file error test

--- GrokBarTests/YAML/YmlParserTests.swift
+++ GrokBarTests/YAML/YmlParserTests.swift
@@ -3,17 +3,16 @@ import XCTest
 
 class YmlParserTests: XCTestCase {
     var ymlParser: YmlParser!
-    var validStubbedParser: YmlParser!
     var testBundle: Bundle!
     var validNgrokStub: URL!
+    var invalidNgrokStub: URL!
     
     override func setUp() {
         super.setUp()
         ymlParser           = YmlParser(path: nil)
         testBundle          = Bundle(for: type(of: self))
         validNgrokStub      = testBundle.url(forResource: "validNgrok", withExtension: "yml")
-        validStubbedParser  = YmlParser(path: nil)
-        validStubbedParser.grokFile = validNgrokStub
+        invalidNgrokStub    = testBundle.url(forResource: "invalidNgrok", withExtension: "yml")
     }
     
     override func tearDown() {
@@ -33,19 +32,27 @@ class YmlParserTests: XCTestCase {
     }
     
     func testValidNgrokFile() {
-        let tunnels = try! validStubbedParser.parseTunnels()
+        ymlParser.grokFile = validNgrokStub
+        let tunnels = try! ymlParser.parseTunnels()
         XCTAssert(tunnels.count == 2)
     }
     
     func testParsingOfTunnels() {
+        ymlParser.grokFile = validNgrokStub
+        
         let tunnels = [
             Tunnel(name: "fake-client", address: 4200, proto: "http", hostName: nil, subDomain: "fake-client"),
             Tunnel(name: "fake-api", address: 3000, proto: "http", hostName: nil, subDomain: "fake-api")
         ]
-        let parsedTunnels = try! validStubbedParser.parseTunnels()
+        let parsedTunnels = try! ymlParser.parseTunnels()
         XCTAssert(tunnels == parsedTunnels)
     }
-    
-    func testParsingDefaultGrokFile(){}
 
+    func testInvalidNgrokFile() {
+        ymlParser.grokFile = invalidNgrokStub
+        XCTAssertThrowsError(try ymlParser.parseTunnels())
+    }
 }
+
+
+
