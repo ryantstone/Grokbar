dc6106c01f2af274aa4a26385a83c8f384af56e1
Ryan Stone <ryan@ryanstone.io>
2018-06-12 18:58:41 -0400

    wip

--- GrokBar/Services/TunnelService.swift
+++ GrokBar/Services/TunnelService.swift
@@ -1,11 +1,15 @@
 import Foundation
 
 class TunnelService {
-
+    
+    let localPath = {
+        return Bundle.main.bundlePath
+    }()
+    
     func startTunnelServer(_ tunnels: [Tunnel]) {
         let task = Process()
         task.launchPath = "/bin/sh"
-        task.arguments = [ "-c", "/usr/local/bin/ngrok start\(tunnelCommand(tunnels))" ]
+        task.arguments = [ "-c", "\(localPath)/Contents/Resources/ngrok start\(tunnelCommand(tunnels))" ]
 
         let pipe = Pipe()
         task.standardOutput = pipe
